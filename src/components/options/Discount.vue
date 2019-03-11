c4bf04e47cdfbf2c2223a8ccef814e8b63036310
annieleonia <annie.onnered@gmail.com>
2019-03-11 12:48:34 +0100

    Functinality to select discount percentage

--- src/components/options/Discount.vue
+++ src/components/options/Discount.vue
@@ -1,21 +1,66 @@
 <template>
   <div>
     <div class="modalHeader">
-      <img class="modalIcon" src="../../assets/discount.svg" alt="icon">
+      <img class="modalIcon" src="../../assets/discount.svg" alt="icon" />
       <h1>Discount</h1>
     </div>
-    <hr>
-
+    <hr />
+    <div class="modalBody">
+      <h2>Select discount</h2>
+      <el-slider v-model="value" show-input :step="5" />
+    </div>
     <div class="modalFooter">
-      <button class="btnCancel">Cancel</button>
-      <button class="btnConfirm">Confirm</button>
+      <button class="btnCancel" v-on:click="close()">Cancel</button>
+      <button class="btnConfirm" v-on:click="confirm()">Confirm</button>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: ["option"],
+  data() {
+    return {
+      value: 0
+    };
+  },
+  methods: {
+    displayDiscount(value) {
+      return value + "% discount";
+    },
+    confirm: function() {
+      if (this.radio != 0) {
+        this.$props.option.edited = true;
+        let returnValue = this.displayDiscount(this.value);
+        this.$props.option.value = returnValue;
+      }
+      this.$emit("close");
+    },
+    close: function() {
+      this.$emit("close");
+    }
+  }
+};
 </script>
 
 <style>
+.modalBody {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  margin: 1em;
+}
+
+.el-slider {
+  margin: 3em 0.1em;
+}
+
+.el-slider__bar {
+  background-color: #409eff;
+}
+.el-slider__button {
+  border-color: #409eff;
+}
+
+h2 {
+  text-align: center;
+}
 </style>
