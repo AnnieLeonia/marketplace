f5f4ca07d6a6aff77679b8af989c2797a68b0205
annieleonia <annie.onnered@gmail.com>
2019-03-12 08:39:29 +0100

    Add temporary product code in promotion for filtering select list

--- src/components/options/Promotion.vue
+++ src/components/options/Promotion.vue
@@ -1,11 +1,27 @@
 <template>
   <div>
     <div class="modalHeader">
-      <img class="modalIcon" src="../../assets/promotion.svg" alt="icon">
+      <img class="modalIcon" src="../../assets/promotion.svg" alt="icon" />
       <h1>Promotion</h1>
     </div>
-    <hr>
-
+    <hr />
+    <div class="modalBody">
+      <el-select v-model="value6" filterable placeholder="Select">
+        <span class="one"> First</span>
+        <span class="one">Second</span>
+        <span class="one">Third</span>
+        <el-option
+          v-for="item in cities"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        >
+          <span class="one">{{ item.value }}</span>
+          <span class="one">{{ item.label }}</span>
+          <span class="one">{{ item.song }}</span>
+        </el-option>
+      </el-select>
+    </div>
     <div class="modalFooter">
       <button class="btnCancel">Cancel</button>
       <button class="btnConfirm">Confirm</button>
@@ -14,8 +30,55 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      cities: [
+        {
+          value: "Beijing",
+          label: "Beijing",
+          song: "Hello"
+        },
+        {
+          value: "Shanghai",
+          label: "Shanghai",
+          song: "Hello again"
+        },
+        {
+          value: "Nanjing",
+          label: "Nanjing",
+          song: "Hey"
+        },
+        {
+          value: "Chengdu",
+          label: "Chengdu",
+          song: "Tjo"
+        },
+        {
+          value: "Shenzhen",
+          label: "Shenzhen",
+          song: "Lol"
+        },
+        {
+          value: "Guangzhou",
+          label: "Guangzhou",
+          song: "Ok"
+        }
+      ],
+      value6: ""
+    };
+  }
+};
 </script>
 
-<style>
+<style >
+.modalBody {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  margin: 1em;
+}
+
+.one {
+  display: inline-grid;
+  width: 100px;
+}
 </style>
