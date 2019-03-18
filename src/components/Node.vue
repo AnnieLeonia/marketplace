5f076d2dff9946ece29e0afb7d2e672c3e01e1ba
annieleonia <annie.onnered@gmail.com>
2019-03-18 15:05:15 +0100

    Code clean in Node.vue

--- src/components/Node.vue
+++ src/components/Node.vue
@@ -1,37 +1,8 @@
 <template>
   <div>
-    <!--
     <draggable
-      class="workspace vertical"
-      v-bind:class="checkEmpty() ? 'filled' : 'empty'"
-      :list="list"
-      :options="settings"
-      @add="add($event)"
-      ref="vert"
-    >
-      <div slot="footer" class="footer">Hello</div>
-      <Option class="option" v-for="option in list" v-bind:key="option.id" :editOption="option"/>
-    </draggable>
-    -->
-    <draggable
-      class="workspace beginning"
-      v-if="!checkEmpty()"
-      :list="list"
-      :options="settings"
-      @add="add()"
-    >
-      <Option
-        class="option"
-        v-for="option in list"
-        v-bind:key="option.id"
-        :editOption="option"
-      />
-      <div slot="footer" class="shadow"></div>
-    </draggable>
-
-    <draggable
-      v-else
-      class="workspace vertical filled"
+      class="workspace"
+      v-bind:class="checkEmpty() ? 'empty' : 'filled'"
       :list="list"
       :options="settings"
       @start="start()"
@@ -46,8 +17,8 @@
         :moved="setDepth(option)"
         :editOption="option"
       />
+      <div class="shadow" v-if="checkEmpty()"></div>
     </draggable>
-
     <draggable class="workspace horizontal" v-if="this.open">
       <Node v-for="i in 2" v-bind:key="i" :id="createID(i)" />
     </draggable>
@@ -93,8 +64,8 @@ export default {
     end() {
       this.$store.state.moving = false;
     },
-    checkEmpty: function() {
-      return this.list.length > 0 || this.$store.state.tree.length === 0;
+    checkEmpty() {
+      return this.list.length === 0 && this.$store.state.tree.length != 0;
     },
     createID: function(i) {
       return this.id + "." + i;
@@ -123,18 +94,11 @@ export default {
   display: flex;
 }
 
-.empty {
-  background-color: rgba(255, 255, 255, 0.3);
-  /*background-color: transparent;*/
-  min-width: 3em;
-  min-height: 1em;
-}
-
 .filled {
   background-color: lightgray;
 }
 
-.beginning .shadow {
+.shadow {
   display: block;
   position: absolute;
   border-radius: 15px;
@@ -144,12 +108,7 @@ export default {
   left: 0;
 }
 
-.beginning .ghostOption + .shadow {
+.empty .ghostOption + .shadow {
   background: lightgray;
 }
-/*
-.empty:hover {
-  background-color: rgba(255, 255, 255, 0.3);
-  background-color: red;
-} */
 </style>
