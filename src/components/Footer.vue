1b377de4ff31584c3322e42fc231c422c7b8e54e
annieleonia <annie.onnered@gmail.com>
2019-03-18 09:38:42 +0100

    Add switch

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,11 +2,29 @@
   <footer>
     <button class="btnCancel">Cancel</button>
     <button class="btnSave">Save</button>
+    <toggle-button
+      :value="onOff"
+      :labels="{ checked: 'On', unchecked: 'Off' }"
+      :css-colors="true"
+      :font-size="20"
+      :width="100"
+      :height="30"
+    />
   </footer>
 </template>
 
 <script>
-export default {};
+import { ToggleButton } from "vue-js-toggle-button";
+export default {
+  components: {
+    ToggleButton
+  },
+  data() {
+    return {
+      onOff: true
+    };
+  }
+};
 </script>
 
  <style scoped>
@@ -21,6 +39,18 @@ footer {
 
 button {
   margin: 12px;
+}
+
+.vue-js-switch {
+  float: right;
+  margin: 13px;
+  border-radius: 15px;
+  background-color: red;
+  filter: drop-shadow(3px 3px 2px rgba(0, 0, 0, 0.7));
+}
+
+.toggled {
+  background-color: green;
 }
 
 .btnSave {
